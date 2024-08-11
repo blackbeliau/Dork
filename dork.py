52bb4d2ffb6c0c973dc2c12b749ce4a51aa058c0
BlackBeliau <123623969+blackbeliau@users.noreply.github.com>
2024-08-12 06:57:08 +0700

    Update dork.py

--- dork.py
+++ dork.py
@@ -1,23 +1,19 @@
 from googlesearch import search
 
 name = input("What to search:")
+
 dorks = [
-    'inlink:', 
-    'intext:', 
-    'intitle:', 
-    'inurl:', 
-    'in:', 
-    'on:'
+    'inlink:', 'intext:', 'intitle:', 'inurl:', 'in:', 'on:'
 ]
 
-try:
-    query = f' {dorks} {name}'
-    print(f"Searching for: {query}")
-    for result in search(f'{query}', num=5, stop=5, pause=5):
-        print(result)
-except Exception as e:
-    print(f"An error occurred: {e}")
-else:
-    print(f"Finished searching for {query}")
-
+for dork in dorks:
+    query = f"{dork} {name}"
+    try:
+        print(f"Searching for: {query}")
+        for result in search(query, num=5, stop=5, pause=5):
+            print(result)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    else:
+        print(f"Finished searching for {query}")
 print("Done")
